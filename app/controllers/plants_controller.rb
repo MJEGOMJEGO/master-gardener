c8a1f56266a6e54b73ca9ad3e09ef4d03f43ba54
Matthieu JEGO <jego_matt@hotmail.com>
2018-12-04 15:37:17 +0100

    Userbenandcontrollerplant

---
+++ app/controllers/plants_controller.rb
@@ -0,0 +1,15 @@
+class PlantsController < ApplicationController
+  before_action :find_plant, only: [:show]
+  def index
+    @plants = Plant.all
+  end
+
+  def show
+  end
+
+  private
+
+  def find_plant
+    @plant = Plant.find(params[:id])
+  end
+end
