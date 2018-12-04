0af669c21ca9643966578c4aeab6f939740d469b
Henri Devorsine <henri-devorsine@hotmail.fr>
2018-12-04 15:51:04 +0100

    add seed species:aaaa plante:sophie

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,20 +13,39 @@ User.destroy_all
 Specie.destroy_all
 
 puts "Creating users..."
-matt = User.create!(email: 'jego_matt@hotmail.com', password: 'password', username: 'Matthieu', city_location: 'Nantes', level: 1, score: 600, img: File.open(Rails.root.join('db/fixtures//users/Matt.png')))
+matt = User.create!(email: 'jego_matt@hotmail.com', password: 'password', username: 'Matthieu', city_location: 'Nantes', level: 1, score: 600, img: File.open(Rails.root.join('db/fixtures/users/Matt.png')))
 
 puts "Creating species..."
 cactus = Specie.create!(name: 'Cactus', max_life_points: 1000 , img_water: File.open(Rails.root.join('db/fixtures/species/cactus/cactus_main.svg')), img_feed: '', img_exposure: '', img_repot: '', img_cutclean:'', img_feeling_good: '', img_feeling_bad: '')
+aaaa = Specie.create!(name: 'aaaa', max_life_points: 2000 , img_water: File.open(Rails.root.join('db/fixtures/species/cactus/cactus_main.svg')), img_feed: '', img_exposure: '', img_repot: '', img_cutclean:'', img_feeling_good: '', img_feeling_bad: '')
 
 
 puts "Creating plants..."
 bernard = Plant.create!(specie: cactus, nickname: 'Bernard', life_points: 600, user: matt)
+sophie = Plant.create!(specie: aaaa, nickname: 'Sophie', life_points: 1, user: matt)
+
 
 puts "Creating actions..."
 water_cactus = Action.create!(name: 'water', points: 100, img: File.open(Rails.root.join('db/fixtures/actions/water.png')), specie: cactus, frequency_in_days: 21 )
 
+
+
+
+water_aaaa = Action.create!(name: 'water', points: 100, img: File.open(Rails.root.join('db/fixtures/actions/water.png')), specie: aaaa, frequency_in_days: 4 )
+cutclean_aaaa = Action.create!(name: 'cut/clean', points: 250, img: File.open(Rails.root.join('db/fixtures/actions/cisors.png')), specie: aaaa, frequency_in_days: 40 )
+
+
+
 puts "Creating tasks..."
 Task.create!(plant: bernard, action: water_cactus, max_date: '2018-12-11', mark_as_done: false)
+
+
+
+
+
+Task.create!(plant: sophie, action: water_aaaa, max_date: '2018-15-11', mark_as_done: false)
+Task.create!(plant: sophie, action: cutclean_aaaa, max_date: '2018-12-16', mark_as_done: false)
+
 
 
 puts "Finished!"
