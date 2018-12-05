39c4865d7ba58ee28dc982abef4268e2af322548
Adrien Bonnet <adrien.pierre.bonnet@gmail.com>
2018-12-05 13:21:50 +0100

    debug seeds and add method helper svg

--- app/helpers/plant_svg_helper.rb
+++ app/helpers/plant_svg_helper.rb
@@ -1,6 +1,6 @@
 module PlantSvgHelper
-  def svg(name)
-    file_path = "#{Rails.root}/app/assets/images/components/#{code}.svg"
+  def svg(file, name)
+    file_path = "#{Rails.root}/app/assets/images/#{file}/#{name}.svg"
     return File.read(file_path).html_safe if File.exists?(file_path)
     '(not found)'
   end
