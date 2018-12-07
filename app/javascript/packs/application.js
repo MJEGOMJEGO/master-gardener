1bddc8af1f74fb19cecf2e4ffe3cab124bb722f4
Benoit de Longeaux <benoitdelongeaux@gmail.com>
2018-12-07 15:09:59 +0100

    dropdown in progress

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,7 @@
 import "bootstrap";
+import "jquery";
+
+menuTrigger = document.querySelector(".js-menu-trigger");
+menuTrigger.classList.toggle("change");
+
+$("#mobile-menu").toggleClass("menu-hidden", 800, "easeOutQuint");
