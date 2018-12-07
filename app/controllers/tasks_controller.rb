c6b3efbf9da5cdad2043f9ca0c697018c0993f9a
Henri Devorsine <henri-devorsine@hotmail.fr>
2018-12-07 13:43:34 +0100

    player_score

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -8,6 +8,7 @@ class TasksController < ApplicationController
     @task.done = true
     @task.save!
     update_plant_lifepoints
+    update_player_score
     redirect_to plant_path(@task.plant)
   end
 
@@ -25,7 +26,10 @@ class TasksController < ApplicationController
     @task.plant.save
   end
 
-  def update_player_points
-    @tasks.plant.user.score += @tasks.plant.user.plants.all.life_points
+  def update_player_score
+    current_user.score = current_user.plants.sum(:life_points)
+    current_user.save!
+    # @task.plant.user.score = @task.plant.user.plants.sum(:life_points)
   end
+
 end
