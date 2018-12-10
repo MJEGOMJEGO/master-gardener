ae74d4ed07aab4291214820a73060d833e3727a0
Henri Devorsine <henri-devorsine@hotmail.fr>
2018-12-10 14:48:19 +0100

    add newbadges to profil/show

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,114 +8,129 @@ class User < ApplicationRecord
 
   BADGES_DATA = {
     'garden-master' => {
-      explanation: 'Garden Master: level 4',
+      explanation: 'Garden Master',
+      description: 'level 4',
       fullfilment_condition: ->(user) { user.score >= 3000 }
     },
     'botanist' => {
-      explanation: 'Botanist : level 3',
+      explanation: 'Botanist',
+      description: 'level 3',
       fullfilment_condition: ->(user) { user.score >= 2000 }
     },
     'green-lover' =>  {
-      explanation: 'Green Lover : level 2',
+      explanation: 'Green Lover',
+      description: 'level 2',
       fullfilment_condition: ->(user) { user.score >= 1000 }
     },
     'novice' => {
-      explanation: 'Novice : level 1',
+      explanation: 'Novice',
+      description: 'level 1',
       fullfilment_condition: ->(user) { user.score >= 500 }
     },
     'amateur' => {
-      explanation: 'Amateur : TWO plants in your garden',
+      explanation:'Amateur',
+      description: 'Two plants in your garden',
       fullfilment_condition: ->(user) { user.plants.count >= 2 }
     },
     'professionnal' => {
-      explanation: 'Pro : THREE plants in your garden',
+      explanation: 'Pro',
+      description: 'Three plants in your garden',
       fullfilment_condition: ->(user) { user.plants.count >= 3 }
     },
     'collector' => {
-      explanation: 'Collector : FIVE plants in your garden',
+      explanation: 'Collector',
+      description: 'five plants in your garden',
       fullfilment_condition: ->(user) { user.plants.count >= 5 }
     },
     'orchidee-genious' => {
-      explanation: '5 good answers on plant orchidee',
+      explanation: 'orchidee-genious',
+      description: '5 good answers on plant orchidee',
       fullfilment_condition: ->(user) { user.username == "fake" }
     },
     'orchidee-master' => {
-      explanation: 'max-level reached on orchidee',
+      explanation: 'orchidee-master',
+      description: 'max-life reached on orchidee',
       fullfilment_condition: ->(user) { user.username == "fake" }
     },
     'gardenia-genious' => {
-      explanation: '5 good answers on plant gardenia',
+      explanation: 'gardenia-genious',
+      description: '5 good answers on plant gardenia',
       fullfilment_condition: ->(user) { user.username == "fake" }
     },
     'gardenia-master' => {
-      explanation: 'max-level reached on gardenia',
+      explanation: 'gardenia-master',
+      description: 'max-life reached on gardenia',
       fullfilment_condition: ->(user) { user.username == "fake" }
     },
     'spathiphyllum-genious' => {
-      explanation: '5 good answers on plant spathiphyllum',
+      explanation: 'spathiphyllum-genious',
+      description: '5 good answers on plant spathiphyllum',
       fullfilment_condition: ->(user) { user.username == "fake" }
     },
     'spathiphyllum-master' => {
-      explanation: 'max-level reached on spathiphyllum',
+      explanation: 'spathiphyllum-master',
+      description: 'max-life reached on spathiphyllum',
       fullfilment_condition: ->(user) { user.username == "fake" }
     },
     'ficus-benjamina-genious' => {
-      explanation: '5 good answers on plant ficus-benjamina',
+      explanation: 'ficus-genious',
+      description: '5 good answers on plant ficus',
       fullfilment_condition: ->(user) { user.username == "fake" }
     },
     'ficus-benjamina-master' => {
-      explanation: 'max-level reached on ficus-benjamina',
+      explanation: 'ficus-master',
+      description: 'max-life reached on ficus',
       fullfilment_condition: ->(user) { user.username == "fake" }
     },
     'guzmania-genious' => {
-      explanation: '5 good answers on plant guzmania',
+      explanation: 'guzmania-genious',
+      description: '5 good answers on plant guzmania',
       fullfilment_condition: ->(user) { user.username == "fake" }
     },
     'guzmania-master' => {
-      explanation: 'max-level reached on guzmania',
+      explanation: 'guzmania-master',
+      description: 'max-life reached on guzmania',
       fullfilment_condition: ->(user) { user.username == "fake" }
     },
     'arroseur' => {
       explanation: 'Watering 10 times',
+      description: 'Watering 10 times',
       fullfilment_condition: ->(user) { user.username == "fake" }
     },
     'cuisinier' => {
-      explanation: 'Fertilize 3 times',
+      explanation: 'cook',
+      description: 'Fertilize 3 time',
       fullfilment_condition: ->(user) { user.username == "fake" }
     },
     'rempoteur' => {
       explanation: 'Repot 2 times',
+      description: 'Repot 2 times',
       fullfilment_condition: ->(user) { user.username == "fake" }
     },
     'bucheron' => {
-      explanation: 'Cut 3 times',
+      explanation: 'lumberjack',
+      description: 'Cut 3 times',
       fullfilment_condition: ->(user) { user.username == "fake" }
     },
     'mister-meteo' =>  {
-      explanation: 'Sun explosure 3 times',
-      fullfilment_condition: ->(user) { user.username == "fake" }
-    },
-    'fake-flower-1' =>  {
-      explanation: 'fake-flower-1',
-      fullfilment_condition: ->(user) { user.username == "fake" }
-    },
-    'fake-flower-2' =>  {
-      explanation: 'fake-flower-2',
+      explanation: 'mister-meteo',
+      description: 'Sun explosure 3 times',
       fullfilment_condition: ->(user) { user.username == "fake" }
     }
   }
 
   class Badge
-    attr_reader :id
-    def initialize(id, explanation, fullfilment_condition)
+    attr_reader :id, :explanation, :description
+    def initialize(id, explanation, description, fullfilment_condition)
       @id                    = id
       @explanation           = explanation
+      @description           = description
       @fullfilment_condition = fullfilment_condition
     end
 
     def self.all
       all_badges_objects = BADGES_DATA.map do |id, information|
-        Badge.new(id, information[:explanation], information[:fullfilment_condition])
+        Badge.new(id, information[:explanation], information[:description], information[:fullfilment_condition])
       end
 
       return all_badges_objects
