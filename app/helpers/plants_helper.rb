722914d83b295d79f6f27f3bf47606eb15d969cb
Adrien Bonnet <adrien.pierre.bonnet@gmail.com>
2018-12-11 11:21:24 +0100

    Display components svg

--- app/helpers/plants_helper.rb
+++ app/helpers/plants_helper.rb
@@ -1,24 +1,30 @@
 module PlantsHelper
-  def svg(folder, specie_name, task_action_name)
+  def plants_svg(specie_name, task_action_name)
     if task_action_name.nil?
-      svg_filename = svg_name_for_action()
+      svg_filename = plants_svg_name_for_action()
     else
-      svg_filename = svg_name_for_action(task_action_name)
+      svg_filename = plants_svg_name_for_action(task_action_name)
     end
 
-    file_path = "#{Rails.root}/app/assets/images/#{folder}/#{specie_name}/#{svg_filename}.svg"
+    file_path = "#{Rails.root}/app/assets/images/plants/#{specie_name}/#{svg_filename}.svg"
     return File.read(file_path).html_safe if File.exists?(file_path)
     '(not found)'
   end
 
-  def svg_name_for_action(action_name = "neutral")
+  def plants_svg_name_for_action(action_name = "neutral")
     case action_name
-    when "water" then "thirsty"
-    when "feed"  then "feed"
-    when "pot"   then "repot"
+    when "water"   then "thirsty"
+    when "feed"    then "feed"
+    when "pot"     then "repot"
     when "neutral" then "neutral"
     else
       "feelgood"
     end
+  end
+
+  def components_svg(component_name)
+    file_path = "#{Rails.root}/app/assets/images/components/#{component_name}.svg"
+    return File.read(file_path).html_safe if File.exists?(file_path)
+    '(not found)'
   end
 end
