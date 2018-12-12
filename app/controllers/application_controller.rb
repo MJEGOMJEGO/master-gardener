e701810329aaa2e6dfc4c93cf658fb1fbbd04871
Benoit de Longeaux <benoitdelongeaux@gmail.com>
2018-12-12 19:24:51 +0100

    record player details at sign-up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   def default_url_options
     { host: ENV["Garden-Master"] || "localhost:3000" }
   end
+
+    protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:city_location, :username])
+  end
+
 end
