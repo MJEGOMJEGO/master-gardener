fa44bbe35a0055901764f5cb4824eb8ac0828375
Benoit de Longeaux <benoitdelongeaux@gmail.com>
2018-12-13 13:28:52 +0100

    animation reviewed

--- db/schema.rb
+++ db/schema.rb
@@ -99,6 +99,8 @@ ActiveRecord::Schema.define(version: 2018_12_13_085433) do
     t.text "badges", default: [], array: true
     t.integer "level_before", default: 0
     t.text "badges_before", array: true
+    t.float "latitude"
+    t.float "longitude"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
