afd8e8b841597752b1d6414c5353e4ae7b516d48
Matthieu JEGO <jego_matt@hotmail.com>
2018-12-13 11:53:26 +0100

    seed and popup badges and badge data

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_12_221522) do
+ActiveRecord::Schema.define(version: 2018_12_12_184810) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -99,8 +99,6 @@ ActiveRecord::Schema.define(version: 2018_12_12_221522) do
     t.text "badges", default: [], array: true
     t.integer "level_before"
     t.text "badges_before", array: true
-    t.float "latitude"
-    t.float "longitude"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
