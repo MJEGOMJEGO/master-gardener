75086254f8847f5273fe325286b1bb150a6dda96
Benoit de Longeaux <benoitdelongeaux@gmail.com>
2018-12-13 16:12:24 +0100

    weather forecasts done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,32 +1,26 @@
+require 'weather_forecast'
+
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
-  before_action :find_user
+  #before_action :find_user
   before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :weather, if: :user_signed_in?
+  before_action :get_weather, if: :user_signed_in?
 
   def default_url_options
     { host: ENV["Garden-Master"] || "localhost:3000" }
   end
 
-    protected
+  protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:city_location, :username])
   end
 
-  def find_user
-    @user = current_user
-  end
+ private
 
-  def weather
-    lat = @user.latitude
-    lg = @user.longitude
-    url = "https://api.darksky.net/forecast/221bd696c0c1e5ae6e52609af6f0ba74/#{lat},#{lg}"
-    user_condiotions = open(url).read
-    conditions =  JSON.parse(user_condiotions)
-    @weather = conditions["currently"]["icon"]
-    @temp = conditions["currently"]["temperature"]
+  def get_weather
+    return unless user_signed_in?
+    @weather = WeatherForecast.get_weather(current_user.latitude, current_user.longitude)
   end
-
 end
