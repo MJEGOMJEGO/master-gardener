a76f04bcac9d92d3c4640fec6bcf993605371008
Benoit de Longeaux <benoitdelongeaux@gmail.com>
2018-12-13 01:03:33 +0100

    weather under progress

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+  before_action :find_user
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :weather, if: :user_signed_in?
 
   def default_url_options
     { host: ENV["Garden-Master"] || "localhost:3000" }
@@ -11,6 +13,20 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:city_location, :username])
+  end
+
+  def find_user
+    @user = current_user
+  end
+
+  def weather
+    lat = @user.latitude
+    lg = @user.longitude
+    url = "https://api.darksky.net/forecast/221bd696c0c1e5ae6e52609af6f0ba74/#{lat},#{lg}"
+    user_condiotions = open(url).read
+    conditions =  JSON.parse(user_condiotions)
+    @weather = conditions["currently"]["icon"]
+    @temp = conditions["currently"]["temperature"]
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,9 @@ class User < ApplicationRecord
   has_many :tasks, through: :plants
   has_many :quizz_answers
   mount_uploader :img, PhotoUploader
+  geocoded_by :city_location
+  after_validation :geocode, if: :will_save_change_to_city_location?
+
+  validates :city_location, presence: true
+
 end
