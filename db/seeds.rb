0004786f8f5cfee30ff01a370fd97ad81f2e1d50
Henri Devorsine <henri-devorsine@hotmail.fr>
2018-12-14 09:58:05 +0100

    seeds tasks done

--- db/seeds.rb
+++ db/seeds.rb
@@ -68,18 +68,24 @@ Task.create!(plant: bernard, action: cut_ficus, max_date: '2018-12-18', status: 
 Task.create!(plant: bernard, action: feed_ficus, max_date: '2018-12-14', status: "pending")
 Task.create!(plant: bernard, action: pot_ficus, max_date: '2018-12-01', status: "deadline_missed")
 Task.create!(plant: bernard, action: exposure_ficus, max_date: '2018-12-31', status: "pending")
+Task.create!(plant: bernard, action: exposure_ficus, max_date: '2018-12-10', status: "done", done_at: '2018-12-09')
+
 
 Task.create!(plant: sophie, action: water_caoutchouc, max_date: '2018-12-25', status: "pending")
 Task.create!(plant: sophie, action: cut_caoutchouc, max_date: '2018-12-18', status: "pending")
 Task.create!(plant: sophie, action: feed_caoutchouc, max_date: '2018-12-20', status: "pending")
 Task.create!(plant: sophie, action: pot_caoutchouc, max_date: '2018-12-14', status: "pending")
 Task.create!(plant: sophie, action: exposure_caoutchouc, max_date: '2018-12-31', status: "pending")
+Task.create!(plant: sophie, action: water_caoutchouc, max_date: '2018-12-07', status: "done", done_at: '2018-12-05' )
+
 
 Task.create!(plant: rené, action: water_cactus, max_date: '2018-12-14', status: "pending")
 Task.create!(plant: rené, action: cut_cactus, max_date: '2018-12-25', status: "pending")
 Task.create!(plant: rené, action: feed_cactus, max_date: '2018-12-25', status: "pending")
 Task.create!(plant: rené, action: pot_cactus, max_date: '2018-12-29', status: "pending")
 Task.create!(plant: rené, action: exposure_cactus, max_date: '2018-12-31', status: "pending")
+Task.create!(plant: rené, action: water_cactus, max_date: '2018-12-05', status: "done", done_at: '2018-12-04')
+
 
 Task.create!(plant: albert, action: water_amaryllis, max_date: '2018-12-14', status: "pending")
 Task.create!(plant: albert, action: cut_amaryllis, max_date: '2018-12-18', status: "pending")
